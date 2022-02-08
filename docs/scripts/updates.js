6e8232e37a6d2f4495b08948c7f188c6e0fd0e05
Chris Blanquera <chris@incept.asia>
2022-02-09 06:28:26 +0800

    erc721b

--- docs/scripts/updates.js
+++ docs/scripts/updates.js
@@ -1,14 +1,21 @@
 (() => {
   const nav = document.querySelectorAll('#nav-update a')
   const content = document.querySelectorAll('div.content')
+
   Array.from(nav).forEach(link => {
     link.addEventListener('click', e => {
       Array.from(nav).forEach(link => link.classList.remove('active'))
       Array.from(content).forEach(content => content.classList.remove('active'))
       link.classList.add('active')
       document.querySelector(link.getAttribute('href')).classList.add('active')
-      e.preventDefault()
-      return false
     })
   })
+
+  if (window.location.hash) {
+    Array.from(nav).forEach(link => {
+      if (link.getAttribute('href') === window.location.hash) {
+        link.click()
+      }
+    })
+  }
 })()
